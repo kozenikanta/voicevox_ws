d28381be6beed821b6c9f31f9798b24bbf7d4d4a
kozenikanta <kanta.kozeni@okadanet.org>
2023-01-22 16:24:06 +0900

    select parser and scraping

--- sample.py
+++ sample.py
@@ -1,15 +1,39 @@
 #import moduel
 from requests_html import HTMLSession
+import re
 
-#Instance
-session = HTMLSession()
+class sample():
+    
+    def request_html(self):
+        #Instance
+        session = HTMLSession()
+        #get URL
+        url = 'https://news.google.com/home?hl=ja&gl=JP&ceid=JP:ja'
+        r = session.get(url)
+        #HTML => TXT
+        r = r.text
+        #out put
+        print(r)
 
-#get URL
-url = 'https://news.google.com/home?hl=ja&gl=JP&ceid=JP:ja'
-r = session.get(url)
 
-#HTML => TXT
-r = r.text
+    def regex(self):
+        text = r'I have a pen'
+        print('######### findall ##########')
+        print(re.findall('a' , text))
 
-#out put
-print(r)
+        print('######### search ##########')
+        print(re.search('I',text))
+        print(re.search('ave',text))
+
+        print('######### match ##########')
+        print(re.match('I', text))
+        print(re.match('ave',text))
+
+if __name__ == "__main__":
+    a = sample()
+    b = input('choise sanple(scraping, regex)')
+    print(b)
+    if b == 'scraping':
+        a.request_html()
+    elif b == 'regex':
+        a.regex()
